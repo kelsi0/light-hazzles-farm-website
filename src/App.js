ecd01717f075392154073b318ac512a16830f993
kelsi0 <kelsi0@live.com>
2022-03-15 08:27:21 +0000

    hook up contentful, set up for home page sections and about page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AboutUs from "./pages/about-us";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import ContactUs from "./pages/contact-us";
+import Booking from "./pages/booking";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/light-hazzles-farm-website" element={<Home />} />
           <Route path="/light-hazzles-farm-website/about-us" element={<AboutUs />} />
           <Route path="/light-hazzles-farm-website/services" element={<Services />} />
+          <Route path="/light-hazzles-farm-website/members" element={<Booking/>} />
           <Route path="/light-hazzles-farm-website/contact-us" element={<ContactUs />} />
         </Routes>
         <Footer />
---
+++ src/pages/booking.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Booking = () => {
+  return (
+    <section style={{ height: '100vh', width: '100%' }}>
+      <iframe
+        title="Booking"
+        src="https://teamup.com/ks3fukpp1xg94od3yq?showProfileAndInfo=0&showSidepanel=1&showAgendaHeader=1&showAgendaDetails=0&showYearViewHeader=1"
+        style={{width: '100%', height: 800, border: '1px solid #cccccc'}} frameBorder="0"/>
+    </section>
+  )
+}
+
+export default Booking;
